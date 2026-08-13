86da5d05b2cc832b60d78fd500d47841c21c8dee
mennaAltear78 <mennaaltear@gmail.com>
2026-08-13 22:01:23 +0300

    Fix CORS

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
-        # "https://your-frontend.vercel.app",
+       "https://credit-fraud-frontend.vercel.app",
     ],    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
