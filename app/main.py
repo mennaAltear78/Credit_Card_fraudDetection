7717716061542e9c885b13d539a8ed7fb9ceaf09
mennaAltear78 <mennaaltear@gmail.com>
2026-08-13 23:14:54 +0300

    Fix CORS

--- app/main.py
+++ app/main.py
@@ -10,10 +10,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
-    #    "https://credit-fraud-frontend.vercel.app",
-	   "https://credit-fraud-frontend-qzp2.vercel.app"
-	#    "https://credit-card-fraud-detection-t7oq.vercel.app"
-	"https://credit-fraud-frontend-1yq8.vercel.app"
+	#    "https://credit-fraud-frontend-qzp2.vercel.app"
+	"https://credit-fraud-frontend-menna78.vercel.app"
     ],    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
